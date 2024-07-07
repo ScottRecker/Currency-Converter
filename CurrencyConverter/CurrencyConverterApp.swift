4a32d040e17c8d9b9b720fe90c1e44ea152f543b
Scott Recker <scott.a.recker@gmail.com>
2024-07-07 17:16:42 -0500

    Add Xcode project

---
+++ CurrencyConverter/CurrencyConverterApp.swift
@@ -0,0 +1,17 @@
+//
+//  CurrencyConverterApp.swift
+//  CurrencyConverter
+//
+//  Created by Scott Recker on 7/7/24.
+//
+
+import SwiftUI
+
+@main
+struct CurrencyConverterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

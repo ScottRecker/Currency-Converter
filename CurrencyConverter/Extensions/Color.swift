37964e6f5caf55cb8a8cc334fe7b8c6372de5469
Scott Recker <scott.a.recker@gmail.com>
2024-07-08 13:27:33 -0500

    Add color extension

---
+++ CurrencyConverter/Extensions/Color.swift
@@ -0,0 +1,12 @@
+//
+//  Color.swift
+//  CurrencyConverter
+//
+//  Created by Scott Recker on 7/7/24.
+//
+
+import SwiftUI
+
+extension Color {
+    static let appColor: Color = Color(UIColor.systemBlue)
+}
